2f18f3e35ee276fa3f395954f23053c4766d2283
Locke Chen <locke12456@gmail.com>
2021-09-07 16:57:20 +0800

    add mpe command

--- generate mpe ts.sh
+++ generate mpe ts.sh
@@ -1,2 +1,4 @@
  #!/bin/bash
  tsp  --verbose --bitrate 30000000 --max-flushed-packets 70 -I null -P regulate --packet-burst 14 -P mpeinject -p 2021 --max-queue 4096 224.200.200.200:9999 -O file test.ts
+ # for low bitrate
+ tsp --verbose --bitrate 10000000 --max-flushed-packets 70 -I null -P regulate --packet-burst 7 -P mpeinject -b 5000000000 -p 2021 --max-queue 4096 224.200.200.200:9999 -O file test2.ts
