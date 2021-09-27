62f0fe0ef90f565e2d720653e708d3f1672405e6
Locke Chen <locke12456@gmail.com>
2021-09-27 15:57:56 +0800

    add delete_segments and list size

--- download_yt2hls.sh
+++ download_yt2hls.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ffmpeg -i $(youtube-dl -f 301 -g  $1) -c:v copy -c:a aac  -f hls -hls_time 10 $2
+ffmpeg -i $(youtube-dl -f 301 -g  $1) -c:v copy -c:a aac  -f hls -hls_time 10 -hls_flags delete_segments -hls_list_size 15  $2
